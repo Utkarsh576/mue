7b801a59456f41aec8670412afbb3653cda90dbc
utkarsh <utkarshptel@gmail.com>
2024-04-01 09:59:18 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/mue/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.mue
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/config/themes.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+final darkThemes =ThemeData.dark(
+  useMaterial3: true,
+);
